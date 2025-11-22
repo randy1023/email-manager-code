8928d95b3f190060a41d5e4d8a62ad3252d1bb23
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-11-22 12:08:15 -0400

    agregar DTOs para el inicio y registro de usuarios con validaciones

---
+++ src/domain/dtos/auth/login-user.dto.ts
@@ -0,0 +1,22 @@
+import { regularExps } from '../../../config'
+
+export class LoginUserDto {
+  private constructor(
+    public readonly email: string,
+    public readonly password: string
+  ) {}
+
+  static create(object: {
+    [key: string]: any
+  }): [string | undefined, LoginUserDto | undefined] {
+    const { email, password } = object
+
+    if (!email) return ['Missing Email', undefined]
+    if (!regularExps.email.test(email)) return ['Invalid Email', undefined]
+    if (!password) return ['Missing Password', undefined]
+    if (password.length < 6)
+      return ['Password must be at least 6 characters', undefined]
+
+    return [undefined, new LoginUserDto(email, password)]
+  }
+}
---
+++ src/domain/dtos/index.ts
@@ -0,0 +1,2 @@
+export * from './auth/register-user.dto'
+export * from './auth/login-user.dto'
