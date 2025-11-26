58c0d2850ce3dfc38550a63daf8ac678cbc495d3
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-11-26 12:11:18 -0400

    agregar soporte para CORS en el servidor y actualizar dependencias relacionadas

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express'
 import path from 'path'
+import cors from 'cors'
 
 interface Options {
   port: number
@@ -25,6 +26,12 @@ export class Server {
     //* Middlewares
     this.app.use(express.json()) // raw
     this.app.use(express.urlencoded({ extended: true })) // x-www-form-urlencoded
+    this.app.use(
+      cors({
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      })
+    )
 
     //* Public Folder
     this.app.use(express.static(this.publicPath))
