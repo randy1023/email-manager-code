94d6e7aab772c475e8a5a8e3655e67a2a13d764c
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-12-12 13:14:37 -0400

    agregar método para crear correos electrónicos por servicio con validaciones

--- src/presentation/services/emails.service.ts
+++ src/presentation/services/emails.service.ts
@@ -1,5 +1,29 @@
+import { EmailModel } from '../../data/mongo/models/email.model'
+import { CreatedEmailServiceDto, CustomError, EmailEntity } from '../../domain'
+
 export class EmailsService {
   constructor() {} // DI
 
   // Aquí puedes agregar métodos relacionados con la gestión de emails
+
+  public async createEmailByService(
+    createdEmailServiceDto: CreatedEmailServiceDto
+  ): Promise<EmailEntity> {
+    // Lógica para crear un email basado en el servicio (netflix,disney,etc )
+    try {
+      const emailByServiceExisted = await EmailModel.findOne({
+        email: createdEmailServiceDto.email,
+        service: createdEmailServiceDto.service,
+      })
+      if (emailByServiceExisted)
+        throw CustomError.badRequest('Email by service already exists')
+
+      const newEmail = new EmailModel(createdEmailServiceDto)
+      await newEmail.save()
+
+      return EmailEntity.fromObject(newEmail)
+    } catch (error) {
+      throw CustomError.internalServerError('Error creating email by service')
+    }
+  }
 }
