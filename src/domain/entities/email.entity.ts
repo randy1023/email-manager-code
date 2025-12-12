61f4ea87f842d56929cac01447180a0f35e2a308
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-12-12 13:00:10 -0400

    mejorar entidad EmailEntity para incluir servicio y fecha de creación, y agregar validaciones correspondientes

--- src/domain/entities/email.entity.ts
+++ src/domain/entities/email.entity.ts
@@ -4,29 +4,38 @@ export class EmailEntity {
   constructor(
     public readonly id: string,
     public readonly email: string,
-    public readonly password: string
+    public readonly password: string,
+    public readonly service: string,
+    public readonly createdAt: Date
   ) {}
 
   get values() {
     const returnObj: { [key: string]: any } = {}
     if (this.id) returnObj.id = this.id
     if (this.email) returnObj.email = this.email
-
+    if (this.service) returnObj.service = this.service
+    if (this.createdAt) returnObj.createdAt = this.createdAt
     return returnObj
   }
 
   static fromObject(object: { [key: string]: any }): EmailEntity {
-    const { id, _id, email, password } = object
+    const { id, _id, email, password, service, createdAt } = object
 
     if (!id && !_id) throw CustomError.badRequest('Missing Id')
     if (!email) throw CustomError.badRequest('Missing Email')
     if (!password) throw CustomError.badRequest('Missing Password')
+    if (!service) throw CustomError.badRequest('Missing Service')
+    if (!createdAt) throw CustomError.badRequest('Missing CreatedAt')
+    if (new Date(createdAt).toString() === 'Invalid Date')
+      throw CustomError.badRequest('Invalid CreatedAt')
 
     return new EmailEntity(
       id || _id,
 
       email,
-      password
+      password,
+      service,
+      new Date(createdAt)
     )
   }
 }
